e9c157eb2d6168f7b6b5f5bd0d8f8bf7a0796a84
Greenessa <romanova184@yandex.ru>
2023-08-20 20:35:30 +0300

    commit about fhotos name as "likes"

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 # Предварительно получается токен для доступа к фотографиям в контакте и OAUTH token для доступа в яндекс диск
 import time
 from tqdm import tqdm
-from progress.bar import IncrementalBar
 import logging
 logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w", format="%(asctime)s %(levelname)s %(message)s")
+
 logging.debug("A DEBUG Message")
 logging.info("An INFO")
 logging.warning("A warning")
@@ -30,14 +30,17 @@ class VK_Client:
         return {'access_token': self.token, 'v': '5.131'}
     def get_photos(self):
         params = self.get_params()
-        params.update({'owner_id': self.user_id})
+        params.update({'owner_id': self.user_id, "extended": 1})
         resp = requests.get(f'{self.API_BASE_URL}/photos.getAll', params=params).json()
         #pprint(resp)
         return resp
+    
+
 token1 = open('token1').read()
 vk_klient_my = VK_Client(token1, 815147892)
 dict = vk_klient_my.get_photos()
 pprint(dict)
+
 
 class Yandex_Client:
 
@@ -54,19 +57,16 @@ class Yandex_Client:
     def upload_photos(self, dict, path_name_folder):
         # загружаем файлы на Яндекс диск
         params = {'path': path_name_folder}
-        #bar = IncrementalBar('Countdown', max=6)
-        #for i in range(0,6):
-        for i, items in tqdm(enumerate(dict['response']['items'])):
-            for size in dict['response']['items'][i]['sizes']:
+        for item in tqdm(dict['response']['items']):
+            for size in item['sizes']:
                 if size['type'] == 'w':
                     #print(size['url'])
-                    params = {'path': f'{path_name_folder}/{i}', 'url': size['url']}
+                    params = {'path': f'{path_name_folder}/{item["likes"]["count"]}', 'url': size['url']}
                     responce = requests.post('https://cloud-api.yandex.net/v1/disk/resources/upload', headers=self.headers, params=params)
                     print(responce.status_code)
                     print(responce.json())
-            # bar.next()
             time.sleep(1)
-        # bar.finish()
+
 
 token = open("token").read()
 yandex_cl1 = Yandex_Client(token)
